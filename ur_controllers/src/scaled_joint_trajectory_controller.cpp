8060538ee9c5facf911cef781ac5751deb8a900c
Nikola Banović <nibanovic@gmail.com>
2023-11-27 17:17:44 +0100

    fixed the delay that was happening when scaling trajectory execution time

--- ur_controllers/src/scaled_joint_trajectory_controller.cpp
+++ ur_controllers/src/scaled_joint_trajectory_controller.cpp
@@ -141,14 +141,26 @@ controller_interface::return_type ScaledJointTrajectoryController::update(const 
   if (has_active_trajectory()) {
     // Main Speed scaling difference...
     // Adjust time with scaling factor
+
     TimeData time_data;
     time_data.time = time;
-    rcl_duration_value_t t_period = (time_data.time - time_data_.readFromRT()->time).nanoseconds();
-    time_data.period = rclcpp::Duration::from_nanoseconds(scaling_factor_ * t_period);
+    
+    // update() has 'period', time since last update call, extract the nanoseconds so it can be scaled
+    rcl_duration_value_t nsec_period = period.nanoseconds();
+    // scale the period                                 
+    time_data.period = rclcpp::Duration::from_nanoseconds(scaling_factor_ * nsec_period);
+    // increment uptime by the scaled period
     time_data.uptime = time_data_.readFromRT()->uptime + time_data.period;
-    rclcpp::Time traj_time = time_data_.readFromRT()->uptime + rclcpp::Duration::from_nanoseconds(t_period);
-    time_data_.reset();
-    time_data_.initRT(time_data);
+
+    // uptime is just a cummulative sum of the *scaled* periods, used for scaled time-ordering of trajectory segments
+    // something like an internal, slowed down clock
+
+    // but for traj_time, increment the uptime by the *unscaled* period - WHY?
+    // this only makes it so the traj_time is a few miliseconds more than the uptime - not noticeable or necessary.
+    rclcpp::Time traj_time = time_data_.readFromRT()->uptime + period;
+
+    // save the as a starting point 
+    time_data_.writeFromNonRT(time_data);
 
     bool first_sample = false;
     // if sampling the first time, set the point before you sample
